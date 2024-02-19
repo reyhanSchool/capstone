1f14ea414f7b10b2601c3c6dac2d062bad38b17e
rambaldeo <77577818+rambaldeo@users.noreply.github.com>
2024-02-19 11:07:31 -0500

    Fixed the IOS bug

--- App.js
+++ App.js
@@ -12,89 +12,78 @@ import CalendarScreen from './components/CalendarScreen';
 const Stack = createStackNavigator();
 
 export default function App() {
-  // Get the current time of day
-  const currentTime = new Date();
-  const currentHour = currentTime.getHours();
-
-  // Example name
-  const name = 'Nived';
-  
   return (
-      <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
-              <Stack.Screen name="Home" component={HomeScreen} />
-              <Stack.Screen name="Medications" component={Medications} />
-              <Stack.Screen name="CalendarScreen" component={CalendarScreen}/>
-          </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Medications" component={Medications} />
+        <Stack.Screen name="CalendarScreen" component={CalendarScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 function HomeScreen({ navigation }) {
-  // Get the current time of day
   const currentTime = new Date();
   const currentHour = currentTime.getHours();
-
-  // Example name
   const name = 'Nived';
 
   return (
-      <View style={styles.container}>
-          <Greeting timeOfDay={currentHour} name={name} />
-          
-          <View style={styles.content}>
-              <RectangleWithImageBackground      
-                  id="Live View"    
-                  title="In Room"
-                  imageUrl={require('./assets/statusBackground.png')}
-                  color="#CCE9C1"
-                  fontSize={35} // Example font size
-                  onPress={() => navigation.navigate('Medications')}// Need to change to the live view page
-              />
-              <View style={styles.row}>
-                  <SquareWithImageBackground
-                      id="Temperature Log"
-                      title="34°"
-                      imageUrl={require('./assets/thermBackground.png')}
-                      color="#E5B9FA"
-                      fontSize={45} // Example font size
-                  />
-                  <SquareWithImageBackground
-                      id="Medication"
-                      title="2 left"
-                      imageUrl={require('./assets/pillBackground.png')}
-                      color="#FFCA99"
-                      fontSize={25} // Example font size
-                      onPress={() => navigation.navigate('Medications')}
-                  />
-              </View>
-              <RectangleWithImageBackground          
-                    id="Appointments"
-                  title="Calendar"
-                  imageUrl={require('./assets/hospitalBackground.png')}
-                  color="#FCADAD"
-                  fontSize={25} // Example font size
-                  onPress={() => navigation.navigate('Calendar')}
-              />
-          </View>
-          <StatusBar style="auto" />
+    <View style={styles.container}>
+      <Greeting timeOfDay={currentHour} name={name} />
+      <View style={styles.content}>
+        <RectangleWithImageBackground
+          id="Live View"
+          title="In Room"
+          imageUrl={require('./assets/statusBackground.png')}
+          color="#CCE9C1"
+          fontSize={35}
+          onPress={() => navigation.navigate('CalendarScreen')} // Adjusted navigation to 'CalendarScreen'
+        />
+        <View style={styles.row}>
+          <SquareWithImageBackground
+            id="Temperature Log"
+            title="34°"
+            imageUrl={require('./assets/thermBackground.png')}
+            color="#E5B9FA"
+            fontSize={45}
+          />
+          <SquareWithImageBackground
+            id="Medication"
+            title="2 left"
+            imageUrl={require('./assets/pillBackground.png')}
+            color="#FFCA99"
+            fontSize={25}
+            onPress={() => navigation.navigate('Medications')} // Adjusted navigation to 'Medications'
+          />
+        </View>
+        <RectangleWithImageBackground
+          id="Appointments"
+          title="Calendar"
+          imageUrl={require('./assets/hospitalBackground.png')}
+          color="#FCADAD"
+          fontSize={25}
+          onPress={() => navigation.navigate('CalendarScreen')} // Adjusted navigation to 'CalendarScreen'
+        />
       </View>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: '#fff',
+    flex: 1,
+    backgroundColor: '#fff',
   },
   content: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   row: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
   },
 });
